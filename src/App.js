34fefbf19fdc1e6e3a6df659a3d3702875ee5d0b
lZarzao <luiszarzao@icloud.com>
2022-07-11 22:53:33 -0400

    Button style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
-import Button from './assets/components/Button';
+import Button from './components/Button';
 import logo from './assets/freecodecamp-logo.png'
 
 function App() {
+
+  const AddCounter = () => {
+    console.log('is clicking')
+  }
+
+  const restartCounter = () => {
+    console.log('restart')
+  }
+
   return (
     <main className='App'>
       <figure className='freecodecamp-logo-container'>
         <img className='freecodecamp-logo' src={logo} alt='freecodecamp logo' />
-        <section className='main-container'>
-          <Button 
-            text='Click'
-            isClicking={true}
-            click={false} />
-          <Button
-            text='Restart'
-            isClicking={false}
-            click={true} />
-        </section>
       </figure>
+      <section className='main-container'>
+        <Button text='Click' isClicking={true} click={AddCounter} />
+        <Button text='Restart' isClicking={false} click={restartCounter} />
+      </section>
     </main>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../stylesheets/Button.css'
 
 function Button({ text, isClicking, click }) {
   return (
